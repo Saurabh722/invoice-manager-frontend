0489d2a737e51e9b88dd07b65ce54cbcbf26a21b
Saurabh Sharma <saurabh4sw@gmail.com>
2024-09-25 08:16:27 +0530

    remove hard code data

--- packages/invoice-manager/src/components/InvoiceApp.js
+++ packages/invoice-manager/src/components/InvoiceApp.js
@@ -1,34 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import CreateProduct from '../widgets/CreateProduct';
 import Table from './Table';
 
-const invoiceData = [
-  {
-    invoiceId:'1',
-    customerId: 'c1',
-    customerName: 'customer1',
-    items: 1,
-    amount: 10,
-  },
-  {
-    id:'2',
-    customerId: 'c2',
-    customerName: 'customer2',
-    items: 2,
-    amount: 20,
-  },
-  {
-    id:'3',
-    customerId: 'c3',
-    customerName: 'customer3',
-    items: 3,
-    amount: 30,
-  }
-]
-
 export default function InvoiceApp() {
-  const [invoices, setInvoices] = useState(invoiceData);
+  const [invoices, setInvoices] = useState([]);
 
   const displayData = invoices.map(({
     customerName,
